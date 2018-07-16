324e7785b7a204924bf1007a1a67c6c5a76dbbdc
saiky93 <38883382+saiky93@users.noreply.github.com>
2018-07-16 11:05:45 -0400

    Scatter-plot

--- src/pages/scatter/scatter.ts
+++ src/pages/scatter/scatter.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {LoginPage} from '../login/login';
 import {CookieService} from 'ngx-cookie-service';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { DeviceService } from '../../services/device.service';
+import { LoadingController } from 'ionic-angular';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 declare var google;
@@ -21,25 +23,113 @@ declare var google;
 export class ScatterPage {
 
   simData:any;
+  payloadData: any[];
+  Temp: number[];
+  Humid: number[];
+  combined: any[];
+  showCenti: boolean;
+  showFahr: boolean;
 
-  constructor(public angularFireDatabase: AngularFireDatabase, public navCtrl: NavController, public cookieService: CookieService, public navParams: NavParams) {
+  constructor(public loadingCtrl: LoadingController, private deviceService: DeviceService, public angularFireDatabase: AngularFireDatabase, public navCtrl: NavController, public cookieService: CookieService, public navParams: NavParams) {
+    this.loading();
+    this.payloadData=[];
+    this.Temp=[];
+    this.Humid=[];
+    this.combined=[];
+    var dateMonthString;
+    var dateDayString;
+    this.showCenti=true;
+    this.showFahr=true;
+    const date = new Date();
+    date.setDate(date.getDate()-7);
+    date.setMonth(date.getUTCMonth()+1);
+    if(date.getUTCMonth()<10)
+    {
+      dateMonthString = "0"+date.getUTCMonth().toString();
+    }else{
+      dateMonthString = date.getUTCMonth().toString();
+    }
+    if(date.getUTCDate()<=9){
+      dateDayString = "0"+date.getUTCDate().toString();
+    }else{
+      dateDayString = date.getUTCDate().toString();
+    }
+    const utcTime = date.getUTCFullYear().toString()+"-"+dateMonthString+"-"+dateDayString+"T00:00:00.000Z";
+    console.log(utcTime);
     if(navParams.get('data')=="0"){
       var simId = '0';
       var macId = this.cookieService.get('machineId');
       console.log(macId);
+      this.deviceService.getPayloadData(macId,utcTime).subscribe((payloads:any[])=>{
+        this.payloadData=payloads['Payloads'];
+        if(this.cookieService.get('unit')=="celsius"){
+          for(var i=0; i<this.payloadData.length; i++){
+            this.Temp[i] = this.payloadData[i].Data.temperature;
+          }
+          for(var j=0; j<this.payloadData.length; j++){
+            this.Humid[j] = this.payloadData[j].Data.humidity;
+          }
+          this.combined[0] = ['Temperature','Humidity'];
+          for(var z=0; z<this.Humid.length; z++){
+            this.combined[z+1] = [this.Temp[z], this.Humid[z]];
+          }
+        }else{
+          for(var i=0; i<this.payloadData.length; i++){
+            this.Temp[i] = (((parseFloat(this.payloadData[i].Data.temperature))*1.8)+32);
+          }
+          for(var j=0; j<this.payloadData.length; j++){
+            this.Humid[j] = this.payloadData[j].Data.humidity;
+          }
+          this.combined[0] = ['Temperature','Humidity'];
+          for(var z=0; z<this.Humid.length; z++){
+            this.combined[z+1] = [this.Temp[z], this.Humid[z]];
+          }
+        }
+      });
     }else{
       var macId = '0';
       var simId = this.cookieService.get('simulatedId');
       console.log(simId);
       this.angularFireDatabase.object('/Device-Data/0/'+simId+'/').valueChanges().subscribe((fireData:any)=>{
         this.simData = fireData;
-        console.log(fireData);
+        if(this.cookieService.get('unit')=="celsius"){
+          for(var i=0; i<this.simData.length; i++){
+            this.Temp[i] = this.simData[i].Temperature;
+          }
+          for(var j=0; j<this.simData.length; j++){
+            this.Humid[j] = this.simData[j].Humidity;
+          }
+          this.combined[0] = ['Temperature','Humidity'];
+          for(var z=0; z<this.Humid.length; z++){
+            this.combined[z+1] = [this.Temp[z], this.Humid[z]];
+          }
+        }else{
+          for(var i=0; i<this.simData.length; i++){
+            this.Temp[i] = (((parseFloat(this.simData[i].Temperature))*1.8)+32);
+          }
+          for(var j=0; j<this.simData.length; j++){
+            this.Humid[j] = this.simData[j].Humidity;
+          }
+          this.combined[0] = ['Temperature','Humidity'];
+          for(var z=0; z<this.Humid.length; z++){
+            this.combined[z+1] = [this.Temp[z], this.Humid[z]];
+          }
+        }
       });
     }
-    console.log(navParams.get('data'));
-    setTimeout(()=>{
-      this.scatterCentigrade();
-    },1000);
+    if(this.cookieService.get('unit')=="celsius"){
+      this.showCenti=false;
+      this.showFahr=true;
+      setTimeout(()=>{
+        this.scatterCentigrade();
+      },4000);
+    }else{
+      this.showCenti=true;
+      this.showFahr=false;
+      setTimeout(()=>{
+        this.scatterFahrenheit();
+      },5000);
+    }
   }
 
   ionViewDidLoad() {
@@ -47,19 +137,24 @@ export class ScatterPage {
   }
 
   scatterCentigrade(){
-    var data = google.visualization.arrayToDataTable([
-      ['Temperature', 'Humidity'],
-      [ 6,      42],
-      [ 34,      45],
-      [ 21,     34],
-      [ 44,      51],
-      [ 30,      33.5],
-      [ 16.5,    17]
-    ]);
+    console.log("scatter centigrade");
+    var data = google.visualization.arrayToDataTable(this.combined,false);
+    var options = {
+      hAxis: {title: 'Temperature', minValue: 0, maxValue: 55,textStyle:{fontSize:15}},
+      vAxis: {title: 'Humidity', minValue: 0, maxValue: 60,textStyle:{fontSize:15}},
+      legend: 'none'
+    };
+    var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
+
+  scatterFahrenheit(){
+    console.log("scatter fahrenheit");
+    var data = google.visualization.arrayToDataTable(this.combined,false);
     var options = {
       title: 'Temperature vs. Humidity',
-      hAxis: {title: 'Temperature', minValue: 0, maxValue: 55},
-      vAxis: {title: 'Humidity', minValue: 0, maxValue: 55},
+      hAxis: {title: 'Temperature', minValue: 0, maxValue: 120,textStyle:{fontSize:15}},
+      vAxis: {title: 'Humidity', minValue: 0, maxValue: 60,textStyle:{fontSize:15}},
       legend: 'none'
     };
     var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
@@ -77,6 +172,22 @@ export class ScatterPage {
       this.navCtrl.setRoot('BarPage', {
         data: "1"
       });
+    }
+  }
+
+  loading(){
+    if(this.cookieService.get('unit')=="celsius"){
+      let load = this.loadingCtrl.create({
+        content:'Loading Please Wait....',
+        duration: 4000
+      });
+      load.present();
+    }else{
+      let load = this.loadingCtrl.create({
+        content:'Loading Please Wait....',
+        duration: 5000
+      });
+      load.present();
     }
   }
 
